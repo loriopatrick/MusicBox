8104f9f8394e2b6586bb908ed83fce7ad3387286
Patrick Lorio <patrick@lorio.me>
2016-04-02 13:27:07 -0700

    Initial commit

---
+++ public/src/index.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var r = require('r-dom');
+
+ReactDOM.render(
+      r.h1('Hello World'),
+      document.getElementById('app')
+);
